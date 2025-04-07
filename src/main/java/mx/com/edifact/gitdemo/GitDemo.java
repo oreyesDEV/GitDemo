2d236b8fbeeb69ee713abb087d2318937be8126a
Roger Azcorra Novelo <edifactmxrogerazcorra@gmail.com>
2025-04-07 12:59:38 -0600

    first commit proyecto java 21

---
+++ src/main/java/mx/com/edifact/gitdemo/GitDemo.java
@@ -0,0 +1,14 @@
+package mx.com.edifact.gitdemo;
+
+/**
+ *
+ * @author ITIC Roger Azcorra Novelo [RJAN]
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World java 21");
+        String name = "Roger Azcorra";
+        System.out.println("Nombre: " + name);
+    }
+}
